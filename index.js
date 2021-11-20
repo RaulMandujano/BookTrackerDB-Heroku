db81acaaaafc8b3a2035ad0fb389cd97b03bcf61
Raul Mandujano <raulmandujanoleon@gmail.com>
2021-11-19 23:07:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config({ path: './server/config/config.env' });
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
